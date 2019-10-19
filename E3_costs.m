bd7ff87d526d3c6ead19bbf3dc9a38f8f144ecfe
jackdonnellan161 <jackdonnellan161@gmail.com>
2019-10-19 14:39:40 -0500

    Looked at non-versioned folder for update times and realized that "E3_tests_2.m" was the proper reference for my cost function.  Seems to work after this update, but I will continue to monitor to make sure the results are fine in my flows.

--- E3_costs.m
+++ E3_costs.m
@@ -2,23 +2,23 @@ function E3 = E3_costs(U_field,V_field,x_dist,y_dist)
     len_x = length(V_field(1,:));
     len_y = length(V_field(:,1));
     E3 = zeros(len_x*len_y*8,3);
-    for ii = 1:len_y
-        for jj = 1:len_x
-            V = [U_field(jj,ii) V_field(jj,ii)];
+    for ii = 1:len_x*len_y
+%             ii
+            V = [U_field(len_y-floor((ii-1)/len_y),mod(ii-1,len_x)+1)...
+                V_field(len_y-floor((ii-1)/len_y),mod(ii-1,len_x)+1)];
             if norm(V) ~= 0
-                uv_cell_array{jj,ii} = V / norm(V);
+                uv_cell_array{ii} = V / norm(V);
             else
-                uv_cell_array{jj,ii} = V;
+                uv_cell_array{ii} = V;
             end
-            up_cell_array{jj,ii} = [0;1];
-            down_cell_array{jj,ii} = [0;-1];
-            right_cell_array{jj,ii} = [1;0];
-            left_cell_array{jj,ii} = [-1;0];
-            up_right_cell_array{jj,ii} = [1;1]./sqrt(2);
-            up_left_cell_array{jj,ii} = [-1;1]./sqrt(2);
-            down_right_cell_array{jj,ii} = [1;-1]./sqrt(2);
-            down_left_cell_array{jj,ii} = [-1;-1]./sqrt(2);
-        end
+            up_cell_array{ii} = [0;1];
+            down_cell_array{ii} = [0;-1];
+            right_cell_array{ii} = [1;0];
+            left_cell_array{ii} = [-1;0];
+            up_right_cell_array{ii} = [x_dist;y_dist]./sqrt(x_dist^2+y_dist^2);
+            up_left_cell_array{ii} = [-x_dist;y_dist]./sqrt(x_dist^2+y_dist^2);
+            down_right_cell_array{ii} = [x_dist;-y_dist]./sqrt(x_dist^2+y_dist^2);
+            down_left_cell_array{ii} = [-x_dist;-y_dist]./sqrt(x_dist^2+y_dist^2);
     end
 
 %     up_costs = cellfun(@(x,y) (-(x*y)+1)*y_dist,uv_cell_array,up_cell_array,...
@@ -55,57 +55,54 @@ function E3 = E3_costs(U_field,V_field,x_dist,y_dist)
     down_left_costs = cellfun(@(x,y) acos(x*y)*sqrt(x_dist^2+y_dist^2),uv_cell_array,down_left_cell_array,...
         'UniformOutput',false);
 
-
+% ii = 1 is bottom left of field, ii = len_x*len_y is top right
     jj = 1;
     for ii = 1:len_x*len_y
-        [x_cost,y_cost] = ind2sub([len_x len_y],...
-            ii);
-            if x_cost ~= len_x %Moving to the Right
-                ind = sub2ind([len_x len_y],x_cost+1,y_cost);
-                cst = right_costs{x_cost,y_cost};
+            if mod(ii,len_x) ~= 0 %Moving Right
+                ind = ii+1;
+                cst = right_costs{ii};
                 E3(jj,:) = [ii ind cst];
                 jj = jj+1;
             end
-            if x_cost ~= 1 %Moving to the Left
-                ind = sub2ind([len_x len_y],x_cost-1,y_cost);
-                cst = left_costs{x_cost,y_cost};
+            if mod(ii,len_x) ~= 1 %Moving to the Left
+                ind = ii-1;
+                cst = left_costs{ii};
                 E3(jj,:) = [ii ind cst];
                 jj = jj+1;
             end
-
-            if y_cost ~= len_y %Moving Up
-                ind = sub2ind([len_x len_y],x_cost,y_cost+1);
-                cst = up_costs{x_cost,y_cost};
+            if ii < ((len_x*(len_y - 1)) + 1) %Moving Up
+                ind = ii + len_x;
+                cst = up_costs{ii};
                 E3(jj,:) = [ii ind cst];
                 jj = jj+1;
             end
-            if y_cost ~= 1 %Moving Down
-                ind = sub2ind([len_x len_y],x_cost,y_cost-1);
-                cst = down_costs{x_cost,y_cost};
+            if ii > len_x %Moving Down
+                ind = ii-len_x;
+                cst = down_costs{ii};
                 E3(jj,:) = [ii ind cst];
                 jj = jj+1;
             end
-            if (x_cost ~= len_x && y_cost ~= len_y) %Moving Up and to the Right
-                ind = sub2ind([len_x len_y],x_cost+1,y_cost+1);
-                cst = up_right_costs{x_cost,y_cost};
+            if ii < ((len_x*(len_y-1)) + 1) &&  mod(ii,len_x) ~= 0 %Moving Up and to the Right
+                ind = ii + len_x + 1;
+                cst = up_right_costs{ii};
                 E3(jj,:) = [ii,ind,cst];
                 jj = jj+1;
             end
-            if (x_cost ~= 1 && y_cost ~= len_y) %Moving Up and to the Left
-                ind = sub2ind([len_x len_y],x_cost-1,y_cost+1);
-                cst = up_left_costs{x_cost,y_cost};
+            if ii < ((len_x*(len_y - 1)) + 1) && mod(ii,len_x) ~= 1 %Moving Up and to the Left
+                ind = ii+len_x-1;
+                cst = up_left_costs{ii};
                 E3(jj,:) = [ii,ind,cst];
                 jj = jj+1;
             end
-            if (x_cost ~= 1 && y_cost ~= 1) %Moving Down and to the Left
-                ind = sub2ind([len_x len_y],x_cost-1,y_cost-1);
-                cst = down_left_costs{x_cost,y_cost};
+            if ii > len_x && ii >= mod(ii,len_x) ~= 1 %Moving Down and to the Left
+                ind = ii-len_x-1;
+                cst = down_left_costs{ii};
                 E3(jj,:) = [ii,ind,cst];
                 jj = jj+1;
             end
-            if (x_cost ~= len_x && y_cost ~= 1) %Moving Down and to the Right
-                ind = sub2ind([len_x len_y],x_cost+1,y_cost-1);
-                cst = down_right_costs{x_cost,y_cost};
+            if ii > len_x && mod(ii,len_x) ~= 0 %Moving Down and to the Right
+                ind = ii-len_x+1;
+                cst = down_right_costs{ii};
                 E3(jj,:) = [ii,ind,cst];
                 jj = jj+1;
             end
