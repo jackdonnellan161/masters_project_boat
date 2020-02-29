149f95515c145ab57111c64590696e8d5dfa9d22
jackdonnellan161 <jackdonnellan161@gmail.com>
2020-02-29 17:48:41 -0600

    Took out loop because it is unneeded

--- monte_carlo_get_points.m
+++ monte_carlo_get_points.m
@@ -1,13 +1,10 @@
 function [start_points, end_points] = monte_carlo_get_points(num_points,field)
-    
     nx = length(field.xs);
     ny = length(field.ys);
-    for ii = 1:num_points
-        x1(ii,:) = unidrnd(nx);
-        y1(ii,:) = unidrnd(ny);
-        x2(ii,:) = unidrnd(nx);
-        y2(ii,:) = unidrnd(ny);
-    end
-    start_points = [field.xs(x1) field.ys(y1)];
-    end_points = [field.xs(x2) field.ys(y2)];
+    x1 = unidrnd(nx,num_points,1);
+    y1 = unidrnd(ny,num_points,1);
+    x2 = unidrnd(nx,num_points,1);
+    y2 = unidrnd(ny,num_points,1);
+    start_points = [field.xs(x1)' field.ys(y1)'];
+    end_points = [field.xs(x2)' field.ys(y2)'];
 end
