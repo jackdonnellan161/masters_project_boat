720b3ff4a39bddffb2c6f31c17650be192b445c0
jackdonnellan161 <jackdonnellan161@gmail.com>
2020-02-29 17:43:17 -0600

    Removed unneeded code, works no issue

--- monte_carlo_get_points.m
+++ monte_carlo_get_points.m
@@ -1,22 +1,13 @@
-num_points = 1;
-for ii = 1:num_points
-    x1(ii,:) = unidrnd(nx);
-    y1(ii,:) = unidrnd(ny);
+function [start_points, end_points] = monte_carlo_get_points(num_points,field)
     
-    x2(ii,:) = unidrnd(nx);
-    y2(ii,:) = unidrnd(ny);
-end
-
-start_points = [x1 y1];
-end_points = [x2 y2];
-% 
-% figure
-% hold on
-% xlim([0 nx])
-% ylim([0 ny])
-for ii = 1:num_points
-   P1 = [start_points(ii,1) start_points(ii,2)];
-   P2 = [end_points(ii,1) end_points(ii,2)];
-   D = P2 - P1;
-%    quiver( P1(1), P1(2), D(1), D(2), 0 )
+    nx = length(field.xs);
+    ny = length(field.ys);
+    for ii = 1:num_points
+        x1(ii,:) = unidrnd(nx);
+        y1(ii,:) = unidrnd(ny);
+        x2(ii,:) = unidrnd(nx);
+        y2(ii,:) = unidrnd(ny);
+    end
+    start_points = [field.xs(x1) field.ys(y1)];
+    end_points = [field.xs(x2) field.ys(y2)];
 end
